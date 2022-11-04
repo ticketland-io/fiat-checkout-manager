68afe7718383eb36daca0f3f28d2dcc51b7b50ab
ppoliani <ppoliani@gmail.com>
2022-11-04 16:40:04 +0200

    feat: imeplement the main fn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use std::{
+  sync::Arc,
+  env,
+  panic,
+  process,
+};
+use actix::prelude::*;
+use amqp_helpers::consumer::consumer_runner::ConsumerRunner;
+use fiat_checkout_manager::{
+  utils::store::Store,
+  queue_consumers::create_checkout::CreateCheckoutHandler,
+};
+
+
 fn main() {
-    println!("Hello, world!");
+  let orig_hook = panic::take_hook();
+  panic::set_hook(Box::new(move |panic_info| {
+    orig_hook(panic_info);
+    process::exit(1);
+  }));
+
+  if env::var("ENV").unwrap() == "development" {
+    dotenv::from_filename(".env").expect("cannot load env from a file");
+  }
+
+
+  let system = System::new();
+
+  let execution = async {
+    let store = Arc::new(Store::new().await);
+
+    let mut role_handler_consumer = ConsumerRunner::new(
+      store.config.rabbitmq_uri.clone(),
+      "checkout_session".to_owned(),
+      "checkout_session".to_owned(),
+      Arc::new(CreateCheckoutHandler::new(store).await),
+    ).await;
+
+    role_handler_consumer.start().await.unwrap();
+  };
+
+  let arbiter = Arbiter::new();
+  arbiter.spawn(execution);
+  system.run().expect("Could not run the actix-rt system");
 }
---
+++ src/queue_consumers/create_checkout.rs
@@ -0,0 +1,33 @@
+use std::sync::Arc;
+use eyre::{Result, Report};
+use tracing::info;
+use async_trait::async_trait;
+use lapin::{
+  message::{Delivery},
+};
+use amqp_helpers::core::types::Handler;
+use crate::{
+  models::create_checkout::CreateCheckout,
+  utils::store::Store,
+};
+
+pub struct CreateCheckoutHandler {
+  store: Arc<Store>
+}
+
+impl CreateCheckoutHandler {
+  pub async fn new(store: Arc<Store>) -> Self {
+    Self {
+      store,
+    }
+  }
+}
+
+#[async_trait]
+impl Handler<CreateCheckout> for CreateCheckoutHandler {
+  async fn handle(&self, msg: CreateCheckout, _: &Delivery) -> Result<()> {
+    info!("Assigning roles to {} in guild {}", &msg.discord_uid, &msg.guild_id);
+
+    Ok(())
+  }
+}
