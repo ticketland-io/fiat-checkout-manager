dd3c3acc421aaae02463e1ec17812948da3ed471
ppoliani <ppoliani@gmail.com>
2022-11-04 16:53:03 +0200

    feat: add ws_session_id to create checkout model

--- src/models/create_checkout.rs
+++ src/models/create_checkout.rs
@@ -2,6 +2,7 @@ use borsh::{BorshSerialize, BorshDeserialize};
 
 #[derive(BorshSerialize, BorshDeserialize, PartialEq, Debug)]
 pub struct CreateCheckout {
+  pub ws_session_id: String,
   pub buyer_uid: String,
   pub sale_account: String,
   pub event_id: String,
