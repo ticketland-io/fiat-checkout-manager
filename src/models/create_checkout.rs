c303b8676513664b4ba99dd10f31485fabd36769
ppoliani <ppoliani@gmail.com>
2022-11-07 16:12:35 +0200

    refactor: change CreateCheckout to enum

--- src/models/create_checkout.rs
+++ src/models/create_checkout.rs
@@ -1,15 +1,25 @@
 use borsh::{BorshSerialize, BorshDeserialize};
 
 #[derive(BorshSerialize, BorshDeserialize, PartialEq, Debug)]
-pub struct CreateCheckout {
-  pub ws_session_id: String,
-  pub buyer_uid: String,
-  pub sale_account: String,
-  pub event_id: String,
-  pub ticket_nft: String,
-  pub ticket_type_index: u8,
-  pub recipient: String,
-  // These two are used in the primary sale checkout not the fill sell listing checkout
-  pub seat_index: Option<u32>,
-  pub seat_name: Option<String>,
+pub enum CreateCheckout {
+  Primary {
+    ws_session_id: String,
+    buyer_uid: String,
+    sale_account: String,
+    event_id: String,
+    ticket_nft: String,
+    ticket_type_index: u8,
+    recipient: String,
+  },
+  Secondary {
+    ws_session_id: String,
+    buyer_uid: String,
+    sale_account: String,
+    event_id: String,
+    ticket_nft: String,
+    ticket_type_index: u8,
+    recipient: String,
+    seat_index: u32,
+    seat_name: String,
+  }
 }
